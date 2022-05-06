4979f79d2a6d32a6c01e60db12b8b2d6f7bad966
takex5g <takex5g@gmail.com>
2022-05-07 00:07:31 +0900

    とりあえず完成

--- pages/index.vue
+++ pages/index.vue
@@ -5,18 +5,22 @@
     @mouseup="touchend($event)"
     @mouseleave="touchend($event)"
   >
-    <div class="bgimg"></div>
-
-    <!-- <div class="mediv"> -->
-    <div
-      class="me"
-      :style="{
-        left: X - 350 + 'px',
-        top: Y - 130 + 'px',
-      }"
-    ></div>
-    <!-- </div> -->
+    <div class="face-div">
+      <img ref="kao" class="face" src="/face.png" width="1292" height="476" />
+      <img
+        ref="me"
+        class="face"
+        src="/me.png"
+        width="1292"
+        height="476"
+        :style="{
+          transform: `translate(calc(${X}px - 50%), calc(${Y}px - 50%))`,
+        }"
+      />
+    </div>
   </div>
+  <!-- 
+          transform: `translate(calc(calc(${X}px - 50%) - 50vw), calc(calc(${Y}px - 50%) - 50vh))`, -->
 </template>
 
 <script lang="ts">
@@ -31,20 +35,19 @@ export default Vue.extend({
   },
   methods: {
     touchmove(e) {
-      this.X = e.x
-      this.Y = e.y
-      // return
-      if (e.x < 486) {
-        if (e.x < 410) this.X = 410
-        else this.X = e.x
-      } else this.X = 486
+      const x = e.x - window.innerWidth / 2
+      const y = e.y - window.innerHeight / 2
 
-      if (e.y < 273) {
-        if (e.y < 136) this.Y = 136
-        else this.Y = e.y
-      } else this.Y = 273
-      console.log('pageX:', e.x, e.y)
-      console.log('touchmove:', e)
+      console.log(e.x, e.y, '動かす:', x, y)
+      if (x < 25) {
+        if (x < -25) this.X = -25
+        else this.X = x
+      } else this.X = 25
+
+      if (y < 39) {
+        if (y < -39) this.Y = -39
+        else this.Y = y
+      } else this.Y = 39
     },
 
     touchend(e) {
@@ -81,33 +84,22 @@ body {
 .bg {
   width: 100%;
   height: 100%;
+  display: flex;
+  justify-content: center;
+  overflow: hidden;
+}
+.face-div {
+  margin: auto;
   position: relative;
-  overflow: hidden;
 }
-.bgimg {
-  background-image: url('/face.png');
-  top: calc(50% - 130px);
-  left: calc(50% - 350px);
-  background-size: contain;
-  background-repeat: no-repeat;
-  width: 100%;
-  max-width: 700px;
-  height: 260px;
-  position: absolute;
-}
-.me {
-  background-image: url('/me.png');
-  top: calc(50% - 130px);
-  left: calc(50% - 350px);
-  background-size: contain;
-  background-repeat: no-repeat;
-  width: 100%;
-  max-width: 700px;
-  height: 260px;
-  position: absolute;
+.face {
   transition-duration: 0.5s;
-}
-.mediv {
-  overflow: hidden;
+  transform: translate(-50%, -50%);
+  transition-timing-function: cubic-bezier(0, 0, 0.06, 0.86);
+  display: block;
+  position: absolute;
+  width: 400px;
+  height: auto;
+  margin: auto;
 }
 </style>
